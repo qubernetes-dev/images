c2b3debea0aaade36cb79e00ae7c33fc0146b9e5
Vlad Stirbu <vstirbu@gmail.com>
2025-05-12 13:07:11 +0300

    Rename artifact root variable to artifacts destination in entrypoint script

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 
 # Default values (can be overridden by environment variables if needed)
 MLFLOW_HOST="${MLFLOW_HOST:-0.0.0.0}"
-MLFLOW_DEFAULT_ARTIFACT_ROOT="${MLFLOW_DEFAULT_ARTIFACT_ROOT:-s3://mlflow}"
+MLFLOW_DEFAULT_ARTIFACTS_DESTINATION="${MLFLOW_DEFAULT_ARTIFACTS_DESTINATION:-s3://mlflow}"
 
 # Start the MLflow server using exec to replace the shell process
 # "$@" allows passing additional arguments from Kubernetes 'args' if needed in the future
 exec mlflow server \
     --host "$MLFLOW_HOST" \
-    --artifacts-destination "$MLFLOW_DEFAULT_ARTIFACT_ROOT" \
+    --artifacts-destination "$MLFLOW_DEFAULT_ARTIFACTS_DESTINATION" \
     "$@"
